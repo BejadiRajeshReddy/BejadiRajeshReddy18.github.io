df90fbd38cec48b1b6773cef9a9f8f3e4bfec60c
Rajesh Reddy Bejadi <64033035+BejadiRajeshReddy18@users.noreply.github.com>
2024-08-14 05:30:48 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -114,7 +114,7 @@ export const experiences = [
     role: "AWS Cloud Intern",
     company: "F13 Technologies",
     date: "Apr 2023 - Jul 2023",
-    desc: "Enhanced user experiences on Neurobit PSG & Hybrid, Portals by resolving bugs & reduced load time by 40%. Built Neurobit Analytics portal using React Js with seamless interaction of REST APIs using AXIOS optimized with React Query. Refactored previous code to TypeScript, updated with new dependency and integrated Vite with Jest for Unit Testing.",
+    desc: "Executed multiple hands-on projects utilizing AWS services such as EC2, S3, CI/CD pipelines, and databases. Demonstrated proficiency in cloud computing technologies through implementation of CI/CD pipelines and infrastructure management. Leveraged AWS services including EC2 for virtual computing, S3 for object storage, and databases for data management, showcasing hands-on expertise in cloud infrastructure.",
     skills: ["EC2", "S3", "CI/CD", "DB"],
   },
   {
@@ -123,7 +123,7 @@ export const experiences = [
     role: "Web Developer Intern",
     company: "LetsGrowMore",
     date: "Nov 2022- Dec 2022",
-    desc: "Built Flipr Connect Platforms using React Js integrated GraphQL with AXIOS, created High level Design and Figma design. Built Backend with GraphQL and Node JS and connected with MongoDb - Reducing API calls by 20%. Dockerized and automated with Kubernetes & Jenkins CI/CD deployed in AWS-EC2 added Prometheus & Grafana for monitoring.",
+    desc: "Developed web applications and completed various projects using HTML, CSS, and JavaScript. Gained hands-on experience in web development by building functional websites, implementing front-end features and user interfaces.",
     skills: ["HTML", "CSS", "JavaScript", "PHP", "MYSQL"],
   },
   {
@@ -132,7 +132,7 @@ export const experiences = [
     role: "Web Developer Intern",
     company: "Solar secure IT Solutions",
     date: "June 2022 - Aug 2022",
-    desc: "Built RudraShakti - Astrologer App, with MVVM Architecture using Java and Android Studio integrating Firebase SDK. Created One to One video call integration with Socket.IO and Firebase cloud functions and integrated with Retrofit. Created Low Level Design and converted Figma design to XML code.",
+    desc: "Executed various web development tasks and projects, enhancing skills in HTML, CSS, and JavaScript. Designed and implemented an e-commerce website with features like product listings, shopping carts, and payment processing, demonstrating proficiency in building end-to-end web applications.",
     skills: ["HTML", "CSS", "JavaScript"],
   },
 ];
@@ -144,7 +144,7 @@ export const education = [
     school: "Malla Reddy University, Hyderabad",
     date: "2020 - 2024",
     grade: "8.47 CGPA",
-    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Kalinga Institute of Industrial Technology, Bhubaneswar. I have completed 4 semesters and have a CGPA of 8.71. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am also a member of the Google Developers Student Club (GDSC) at KIIT, where I am learning and working on exciting projects with a team of talented developers.",
+    desc: "Obtained a Bachelor's degree in Computer Science from Malla Reddy University Hyderabad. Acquired comprehensive knowledge in core computer science concepts, programming languages, data structures, algorithms, and software development methodologies. Completed relevant coursework in areas such as Data Structures and Algorithms, Design and Analysis of Algorithms, Software Tools, Internet Fundamentals, and Computer Networks. Graduated with a CGPA of 8.47/10.",
     degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
   },
   {
@@ -153,7 +153,7 @@ export const education = [
     school: "Yadadri Junior College, Hyderabad",
     date: "2018 - 2020",
     grade: "91.3%",
-    desc: "I completed my class 12 high school education at Methodist School, Dankuni, where I studied Science with Computer Science.",
+    desc: "Completed higher secondary education at Yadadri Junior College, Hyderabad, establishing a robust academic foundation. Demonstrated strong academic performance, securing an aggregate of 91.3%.",
     degree: "Class XII",
   },
   {
@@ -162,7 +162,7 @@ export const education = [
     school: "Veda High School, Kondapak, Siddipet",
     date: "2017 - 2018",
     grade: "95%",
-    desc: "I completed my class 10 education at Methodist School, Dankuni, where I studied Science with Computer Application.",
+    desc: "Completed secondary education at Veda High School, Kondapak, Siddipet, with excellent academic performance, securing an aggregate of 95%.",
     degree: "Class X",
   },
 ];
@@ -170,27 +170,27 @@ export const education = [
 export const projects = [
   {
     id: 11,
-    title: "E-commerce website",
-    date: "Jan 2024 - Dec 2023",
+    title: "GitOps Pipeline with ArgoCD",
+    date: "April 2024",
     description:
-      "A Rule Builder application “Decision Hub” that empowers Business Analysts to create, save, and visualize decision strategies. Provide a no-code rule writing experience and visual representation to test these rules in real-time and observe the calculations at each step.",
+      "Implemented a GitOps pipeline using Argo CD and Argo Rollouts for continuous deployment of a web application on Kubernetes. Leveraged Argo CD for declarative application deployments and Argo Rollouts for canary releases. Enabled safe and gradual rollouts of new versions by updating manifests in Git and triggering deployments in the cluster. Automated the entire process, including building Docker images, pushing to a registry, and deploying to Kubernetes.",
     image:
-      "https://github.com/rishavchanda/DecisionHub/raw/master/assets/testRule.jpg",
-    tags: ["HTML", "CSS", "JavaScript", "Nodejs"],
+      "https://miro.medium.com/v2/resize:fit:828/format:webp/1*BqhBVtvV8233x6WdlaLOeQ.png",
+    tags: ["ArgoCD", "Docker", "Kubernetes", "Minikube", "Github", "Argo Rollouts", "kubectl", "Python", "Flask"],
     category: "web app",
-    github: "https://github.com/rishavchanda/DecisionHub",
-    webapp: "https://decisionhub.netlify.app/",
+    github: "https://github.com/BejadiRajeshReddy18/GitOps-Pipeline-with-ArgoCD.git",
+    webapp: "https://bejadirajeshreddy18.github.io/GitOps-Pipeline-with-ArgoCD/",
   },
   {
     id: 12,
     title: "End to End CI/CD Project",
-    date: "Jan 2024 - Dec 2023",
+    date: "Sep 2023",
     description:
-      "Build Continuous integration & Continuous pipeline using jenkins. Writing Docker files for docker images to be used for application deployment. Manage Source code Repository with GIT. Integrate Code Quality tolls like SonarQube",
+      "Established a continuous integration and continuous deployment pipeline using Jenkins. Wrote Docker files for Docker images to be used for application deployment. Managed the source code repository with Git and integrated code quality tools like SonarQube.",
     image:
-      "https://github.com/rishavchanda/DecisionHub/raw/master/assets/testRule.jpg",
+      "https://github.com/BejadiRajeshReddy18/Project01/blob/main/CICD.png",
     tags: [
-      " Linux",
+      "Linux",
       "Jenkins",
       "Maven",
       "SonarQube",
@@ -200,19 +200,19 @@ export const projects = [
       "Trivy",
     ],
     category: "web app",
-    github: "https://github.com/rishavchanda/DecisionHub",
-    webapp: "https://decisionhub.netlify.app/",
+    github: "https://github.com/BejadiRajeshReddy18/Project01.git",
+    webapp: "https://github.com/BejadiRajeshReddy18/Project01.git",
   },
   {
     id: 13,
-    title: "Streamlit Powered research intelligence tool",
+    title: "Streamlit Powered Investment Research Intelligence Tool",
     date: "April 2022",
     description:
-      "Developed a website that provide online courses. The admin need to activate the courses to the users. The user needs to register andloginthroughthe idandenrollinthe courses.",
+      "RockyBot is a user-friendly news research tool designed for effortless information retrieval. Users can input article URLs and ask questions to receive relevant insights from the stock market and financial domain.",
     image:
-      "https://user-images.githubusercontent.com/64485885/255202416-e1f89b04-2788-45b0-abc2-9dec616669e2.png",
+      "https://github.com/BejadiRajeshReddy18/llm/raw/main/2_news_research_tool_project/rockybot.jpg",
     tags: ["Streamlit", "Python", "OpenAI", "LangChain"],
-    github: "https://github.com/rishavchanda/Trackify",
-    webapp: "https://trackify-management.netlify.app/",
+    github: "https://github.com/BejadiRajeshReddy18/llm.git",
+    webapp: "https://github.com/BejadiRajeshReddy18/llm.git",
   },
 ];
